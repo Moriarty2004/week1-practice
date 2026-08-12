94cecd2a68de1588c4eb1588172a6b1873531a9b
shreyansh gupta <128899240+Moriarty2004@users.noreply.github.com>
2026-08-12 12:35:59 +0530

    Refactor bill summary print statement

--- question1.py
+++ question1.py
@@ -1,26 +1,18 @@
-# Get input from user
+
 name = input("Enter customer name: ")
 units = float(input("Enter total units used: "))
-
-# Calculate base charge step by step
 if units <= 100:
     bill = units * 2
 elif units <= 200:
     bill = (100 * 2) + ((units - 100) * 3)
 else:
     bill = (100 * 2) + (100 * 3) + ((units - 200) * 5)
-
-# Check if surcharge applies
 if bill > 1000:
     surcharge = bill * 0.05
 else:
     surcharge = 0
-
-# Calculate final total
 total = bill + surcharge
-
-# Print results
-print("\n--- BILL SUMMARY ---")
+print("\nBILL SUMMARY")
 print("Customer Name:", name)
 print("Units Consumed:", units)
 print("Electricity Charge: ₹", bill)
